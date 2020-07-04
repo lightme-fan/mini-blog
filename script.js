060392053efc48882183b561010a4d25ad9cb4e8
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-04 09:41:26 +0100

    Grabbing some element to the dome and adding the new post

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 console.log('it works');
 
 // code your solution in here
+
+const postList = document.getElementById('post-list');
+const card = document.querySelector('.card');
+const cardBody = document.querySelector('.card-body');
+const formGroup = document.querySelector('.form-group');
+const newPostLine = document.querySelector('.new-post-line');
+const newPostTitle = document.querySelector('#new-post-title');
+console.log(newPostTitle);
+const newPostContent = document.querySelector('#new-post-content');
+console.log(newPostContent);
+
+const newPostHeading = document.createElement('h5');
+newPostHeading.classList.add('new-post-title');
+newPostHeading.textContent = 'My article';
+newPostTitle.appendChild('newPostHeading');
+
+const newPostText = document.createElement('p');
+newPostText.classList.add('new-post-text');
+newPostText.textContent = `A hobby is a regular activity done for enjoyment, typically during one's leisure time, not professionally and not for pay. Hobbies include collecting themed items and objects, engaging in creative and artistic pursuits, playing sports, or pursuing other amusements.`;
+newPostContent.appendChild('newPostText');
+
