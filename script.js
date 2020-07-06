b258b2d1633f855da8ebe07ba69af03ffcb64fef
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-06 07:59:13 +0100

    Finished exercise

--- script.js
+++ script.js
@@ -20,42 +20,52 @@ const card = document.createElement('div');
 const newCardBody = document.createElement('div');
 const newPostHeading = document.createElement('h5');
 const newPostText = document.createElement('p');
+const firstNewSpan = document.createElement('span');
+const secondNewSpan = document.createElement('span');
+const thirdNewSpan = document.createElement('span');
+
+// Checkbox content
+firstNewSpan.textContent = 'hobbies';
+secondNewSpan.textContent = 'School Life';
+thirdNewSpan.textContent = 'Personal';
 
 // adding classes for the new elements
 card.classList.add('card', 'some-space');
 newCardBody.classList.add('card-body');
 newPostHeading.classList.add('card-title');
 newPostText.classList.add('card-text');
+firstNewSpan.classList.add('badge', 'badge-light', 'hidden');
+secondNewSpan.classList.add('badge', 'badge-light', 'hidden');
+thirdNewSpan.classList.add('badge', 'badge-light', 'hidden');
+
 
 // pushing the new elements in to the HTML
 newCardBody.appendChild(newPostHeading);
 newCardBody.appendChild(newPostText);
+newCardBody.appendChild(firstNewSpan);
+newCardBody.appendChild(secondNewSpan);
+newCardBody.appendChild(thirdNewSpan);
 card.appendChild(newCardBody);
 postList.appendChild(card);
+console.log(card);
 
 // Event listner for submit button
 submit.addEventListener('click', ($event) => {
     $event.preventDefault();
     newPostHeading.textContent = `${title.value}`;
     newPostText.textContent = `${content.value}`;
+
+    if (hobbies.checked) {
+        firstNewSpan.classList.remove('hidden');
+    }
+
+    if (schoolLife.checked) {
+        secondNewSpan.classList.remove('hidden');
+    }
+
+    if (personal.checked) {
+        thirdNewSpan.classList.remove('hidden');
+    }
+
     document.querySelector('form').reset();
-});
-
-// Event listner for checkbox
-hobbies.addEventListener('change', ($event) => {     
-    if ($event.target.checked === true) {
-        span.appendChild(hobbies);
-    } 
-});
-
-schoolLife.addEventListener('change', ($event) => {     
-    if ($event.target.checked === true) {
-        span.appendChild(schoolLife);
-    } 
-});
-
-personal.addEventListener('change', ($event) => {     
-    if ($event.target.checked === true) {
-        span.appendChild(personal);
-    } 
 });
